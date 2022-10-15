69051032bfa1e481a10eca0f960b44e1ff790984
Melagas <106124711+Melagas@users.noreply.github.com>
2022-10-15 17:24:25 -0300

    se utilizó y puso en funcionamiento firestore

--- src/App.js
+++ src/App.js
@@ -6,10 +6,46 @@ import  { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ItemDetailContainer from './components/ItemDetail/ItemDetailContainer';
 import Cart from './components/Cart/Cart';
 import Checkout from './components/Checkout';
-
-
+import { useEffect } from 'react';
+import {doc, getFirestore, getDoc, collection, getDocs } from 'firebase/firestore';
 
 function App() {
+  //acceso a un documento especifico -> detail
+  useEffect(()=> {
+    const database =getFirestore();
+    const itemReference = doc(database, 'items', '3vnjE6HDrJGMCvxoOsNQ');
+    getDoc(itemReference)
+    .then((snapshot)=>{
+      if(snapshot.exists()){
+      const item = {
+        id: snapshot.id,
+        ...snapshot.data()
+      };
+      console.log (item);
+    }
+    })
+    .catch(error => console.warn(error))
+  }, []);
+
+  //acceso a una coleccion -> list
+  useEffect(()=> {
+    const database = getFirestore();
+
+    const collectionReference = collection(database, 'items');
+
+    getDocs(collectionReference)
+    .then((snapshot)=>{
+const list = snapshot
+.docs
+.map((doc) =>({
+id: doc.id,
+...doc.data()
+}));
+console.log (list);
+})
+    .catch(error => console.warn(error))
+  }, [])
+
   return (
     <BrowserRouter basename='/ecommerce-react'>
 <NavBar />
@@ -22,6 +58,6 @@ function App() {
 </Routes>
     </BrowserRouter>
   );
-}
+  }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { initializeApp } from "firebase/app";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBVskt-foIPO3wTRRoqnRx3kiz-ucj6EQY",
+  authDomain: "ceramica-mela.firebaseapp.com",
+  projectId: "ceramica-mela",
+  storageBucket: "ceramica-mela.appspot.com",
+  messagingSenderId: "802078097604",
+  appId: "1:802078097604:web:ac22d86a229095233cc991"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
